e3f6f0ce7e73a8a751cea002f7bec691a069d821
hector <hectoreduardo354@hotmail.com>
2018-09-21 13:19:41 -0500

    tarea matriz

---
+++ Semana 1/Miercoles/Matriz.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Matriz{
+
+
+	Scanner teclado = new Scanner(System.in);
+	public void cMatriz(int orden){
+		int i,j;
+		}
+	}
+
+	public void PeMatriz1(int orden){
+				matriz_a.get(i).add(this.teclado.nextDouble());
+			}
+		}
+	}
+
+	public void PeMatriz2(int orden){
+				System.out.println("Ingrese el elmento" + "[" + (i+1) + "]" + "[" + (j+1) + "]");
+				matriz_b.get(i).add(this.teclado.nextDouble());
+				matriz_r.get(i).add(0.0);
+			}
+		}
+	}
+
+	public void pMatriz(int orden){
+		int i,j;
+		System.out.println("\nSus matrices son\n***Matriz 1***\n");
+		for(i=0;i<orden;i++){
+		}
+
+		System.out.println("***Matriz 2***\n");
+
+		for(i=0;i<orden;i++){
+			for(j=0;j<orden;j++){
+				System.out.print(matriz_b.get(i).get(j) + "\t");
+			}
+			System.out.println("\n");
+		}
+	}
+
+	public void impMat(int orden){
+		int i,j;
+
+
+		for(i=0;i<orden;i++){
+			for(j=0;j<orden;j++){
+
+		}
+	}
+
+
+	public void suma(int orden){
+		int i,j;
+		for (i=0;i<orden;i++ ) {
+
+		}
+	}
+
+	public void resta(int orden){
+		int i,j;
+		for (i=0;i<orden;i++ ) {
+			for
+			}
+		}
+	}
+
+	public void multi(int orden){
+		int i,j;
+		Double a = new Double(0.0);
+
+
+		}
+	}
+
+	public void trans(int orden){
+		int i,j;
+		System.out.println("\nMatriz Transpuesta");
+				System.out.print(matriz_r.get(i).get(j) + "\t");
+		
+	}
+
+
+}
