59a1dec0a50e1d6265dacb4b0387df8464957a94
hector <hectoreduardo354@hotmail.com>
2018-09-23 18:08:25 -0500

    Añadí la tarea que se entrega mañana lunes que es de la clase inservible y la clase prueba.+

---
+++ Semana 2/Lunes/Tarea que se entrega este dia/Parte 4/ClaseInservible.java
@@ -0,0 +1,9 @@
+public final class ClaseInservible extends Object{
+  /*
+    - Si una clase se marca como final, no se podrá extender la clase.
+  */
+
+  private ClaseInservible(){
+    //Al hacer mi constructor privado lo que logro es que niguna clase de primer nivel pueda hacer uso de ella.
+  }
+}
---
+++ Semana 2/Lunes/Tarea que se entrega este dia/Parte 4/ClasePrueba.java
@@ -0,0 +1,5 @@
+public class ClasePrueba extends ClaseInservible{
+  //Al hacer a mi ClaseInservible fuera final me aseguré de que niguna otra clase pudiera extender de ella.
+  ClaseInservible alg = new ClaseInservible();
+
+}
